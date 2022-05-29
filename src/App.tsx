3c8df8aaa506bb61e0893b2c88e6dc27b6069622
Jeremy Greer <jex.grizzle@gmail.com>
2022-05-29 13:47:40 -0400

    Compare element strengths

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
 
 enum CardType {
   air,
@@ -15,12 +15,25 @@ const cardTypes: CardType[] = Object.values(CardType)
 
 type CardProps = {
   cardType: CardType
+  combatResult: CombatResult | null,
+}
+
+enum CombatResult {
+  win,
+  lose,
+  draw,
 }
 
 const Card: React.FunctionComponent<CardProps> = (props: CardProps) => {
+  const {
+    cardType,
+    combatResult,
+  } = props
   return (
     <div className="Card">
-      {CardType[props.cardType]}
+      {CardType[cardType]}
+      <br />
+      {combatResult !== null && CombatResult[combatResult]}
     </div>
   )
 }
@@ -50,9 +63,36 @@ const initialState: State = {
   ]
 }
 
+const areOpposites = (a: CardType, b: CardType): boolean => {
+  return (
+    (a === CardType.air && b === CardType.earth)
+    || (a === CardType.fire && b === CardType.water)
+    || (a === CardType.earth && b === CardType.air)
+    || (a === CardType.water && b === CardType.fire)
+  )
+}
+
+const isStronger = (a: CardType, b: CardType): boolean => {
+  //  A < F < E < W < A
+  return (
+    (a === CardType.air && b === CardType.water)
+    || (a === CardType.fire && b === CardType.air)
+    || (a === CardType.earth && b === CardType.fire)
+    || (a === CardType.water && b === CardType.earth)
+  )
+}
+
+const getCombatResult = (a: CardType, opponent: CardType): CombatResult => {
+  if (a === opponent || areOpposites(a, opponent)) {
+    return CombatResult.draw
+  }
+  return isStronger(a, opponent)
+    ? CombatResult.win
+    : CombatResult.lose
+}
+
 const App = () => {
   const [state, setState] = useState<State>(initialState)
-
   const { players} = state
   const replacePlayer = (replaceIndex: number, replacePlayer: Player) => {
     const newPlayers = players.map((player, i) => {
@@ -80,10 +120,18 @@ const App = () => {
             cardType: nextType,
           })
         }
+
+        const opponent = players.filter((_, playerIndex) => playerIndex !== i)[0]
+
+        const combatResult = getCombatResult(player.cardType, opponent.cardType)
+
         return (
           <div key={i} className="Row">
             <button onClick={handleChangeType}>change type</button>
-            <Card cardType={player.cardType} />
+            <Card
+              cardType={player.cardType}
+              combatResult={combatResult}
+            />
           </div>
         )
       })}
@@ -91,4 +139,5 @@ const App = () => {
   );
 }
 
-export default App;
+export default App
+

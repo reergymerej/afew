5e93178b94af0a5b3c76c7b5f65a98e3be5019d8
Jeremy Greer <jex.grizzle@gmail.com>
2022-05-29 10:02:20 -0400

    Add ability to change card types

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 enum CardType {
@@ -12,7 +13,7 @@ type CardProps = {
   cardType: CardType
 }
 
-const Card = (props: CardProps) => {
+const Card: React.FunctionComponent<CardProps> = (props: CardProps) => {
   return (
     <div className="Card">
       {CardType[props.cardType]}
@@ -20,15 +21,74 @@ const Card = (props: CardProps) => {
   )
 }
 
-function App() {
+type Player = {
+  cardType: CardType,
+}
+
+const getNextType = (cardType: CardType): CardType => {
+  const types: CardType[] = [
+    CardType.air,
+    CardType.fire,
+    CardType.earth,
+    CardType.water,
+  ]
+  const currentIndex = types.indexOf(cardType)
+  const nextIndex = (currentIndex + 1) % types.length
+  return types[nextIndex]
+}
+
+type State = {
+  players: Player[],
+}
+
+const initialState: State = {
+  players: [
+    {
+      cardType: CardType.fire,
+    },
+    {
+      cardType: CardType.earth,
+    },
+  ]
+}
+
+const App = () => {
+  const [state, setState] = useState<State>(initialState)
+
+  const { players} = state
+  const replacePlayer = (replaceIndex: number, replacePlayer: Player) => {
+    const newPlayers = players.map((player, i) => {
+      if (i === replaceIndex) {
+        return replacePlayer
+      }
+      return player
+    })
+    const nextState = {
+      ...state,
+      players: newPlayers,
+    }
+    setState(nextState)
+  }
+
   return (
     <div className="App">
-      <div className="Row">
-        <Card cardType={CardType.air} />
-      </div>
-      <div className="Row">
-        <Card cardType={CardType.fire} />
-      </div>
+      { players.map((player, i) => {
+        const handleChangeType = () => {
+          const nextType = getNextType(player.cardType)
+          const playerIndex = players.indexOf(player)
+          replacePlayer(playerIndex,
+            {
+            ...player,
+            cardType: nextType,
+          })
+        }
+        return (
+          <div key={i} className="Row">
+            <button onClick={handleChangeType}>change type</button>
+            <Card cardType={player.cardType} />
+          </div>
+        )
+      })}
     </div>
   );
 }

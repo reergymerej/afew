15fc9f0bec6c995e5ec43b72f980ba1ce7bf78f3
Jeremy Greer <jex.grizzle@gmail.com>
2022-06-03 09:32:34 -0400

    Add rolling state to dice

--- src/Die.tsx
+++ src/Die.tsx
@@ -13,15 +13,38 @@ export type DieProps = {
 }
 
 const Die: React.FunctionComponent<DieProps> = (props: DieProps) => {
-  const { value, onRoll, canRoll} = props
+  const [rolling, setRolling] = React.useState(false)
+  const {value, onRoll, canRoll} = props
+  const [rollingValue, setRollingValue] = React.useState<number>(0)
+  const ROLL_DURATION = 1500
+  const ROLL_SPEED = 100
+  const ref = React.useRef<number>(rollingValue)
+
+  // XXX: sloppy effects
   const handleClick = () => {
-    if (canRoll) {
-      const newValue = rand(1, 6)
-      onRoll(newValue)
+    if (canRoll || !rolling) {
+      setRolling(true)
+      setRollingValue(rand(1, 6))
+      const rollingInterval = setInterval(() => {
+        setRollingValue(rand(1, 6))
+      }, ROLL_SPEED)
+      setTimeout(() => {
+        clearInterval(rollingInterval)
+        setRolling(false)
+        onRoll(ref.current)
+      }, ROLL_DURATION)
     }
   }
+
+  React.useEffect(() => {
+    ref.current = rollingValue
+  }, [rollingValue])
+
   return (
-    <div className="Die" onClick={handleClick}>{value}</div>
+    <div className="Die" onClick={handleClick}>
+      {rolling && <span>{rollingValue}</span>}
+      {!rolling && value}
+    </div>
   )
 }
 
